9df71db00571dfcd6acafdd58c7f4e70364cce62
giovanna do nascimento <giovannafernandes134@gmail.com>
2019-11-13 08:57:20 -0300

    nivel do jogo

---
+++ balão.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Jogo dos blaões</title>
+</head>
+<body>
+	<div>
+		<p>
+				<b>NÍVEL:</b>
+				<select name="nivel" id="jogo"></select>
+				<option value="1">FÁCIL</option>
+				<option value="2">MÉDIO</option>
+				<option value="3">DIFÍCIL</option>
+		</p>
+	</div>
+	
+</body>
+</html>
