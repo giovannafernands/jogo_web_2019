f2ac3c492bc9bb036cc88c80e5600347b304ee55
giovanna do nascimento <giovannafernandes134@gmail.com>
2019-12-09 20:43:08 -0300

    ajustes

--- balloon.js
+++ balloon.js
@@ -1,110 +1,115 @@
-var tempoId = null;
+var timerId = null; 
 
-function playgame() {
-	var url = window.location.search;
-	var nivel  = url.replace("?", "");
-	var tempo_seg = 0;
+function iniciaJogo(){
+    var url = window.location.search;
+    var nivel_jogo = url.replace("?", "");
+    var tempo_segundos = 0;
 
- //tempo nível 1 -> fácil
- if (nivel  ==1) {
- 	tempo_seg = 160;
- } 
-//tempo nível 2 -> médio
-if (nivel == 2)  {
-	tempo_seg = 80;
-}
+    //1 fácil 
+    if(nivel_jogo == 1){
+        tempo_segundos = 120;
+    }
+    //2 médio
+    if(nivel_jogo == 2){
+        tempo_segundos = 60;
+    }
 
-//tempo nível 3 -> difícil
-if (nivel == 3) {
-	tempo_seg == 40;
-}
+    //3 difícil
+    if(nivel_jogo == 3){
+        tempo_segundos = 30;
+    }
 
+    // inserindo segundos
+    document.getElementById('cronometro').innerHTML = tempo_segundos;
 
-document.getElementByid('cronometro').innertHTML = tempo_seg;
+    // quantidade de balões
+    var qtde_baloes = 84;
 
-//quantidade de balloon
-var quantidade_balloon = 80;
+    cria_baloes(qtde_baloes);
 
-criar_baloes(quantidade_balloon);
+    //imprimir quantidade de balões inteiros
+    document.getElementById('balloon_inteiros').innerHTML = qtde_baloes;
 
-//quantidade de balloon inteiro
-document.getElementByid('balloon_int').innerHTML = quantidade_balloon;
+    //imprimir quantidade de balões estourados
+    document.getElementById('balloon_estourados').innerHTML = 0;
 
-//imprimir balloon estourados
-document.getElementByid('balloon_est').innerHTML = 0;
-
-contagem_tempo(tempo_seg + 1);
+    contagem_tempo(tempo_segundos + 1);
 
 }
 
 function contagem_tempo(segundos){
-	segundos = segundos -1;
+    segundos = segundos - 1;
 
-	if(segundos == -1){
-		clearTimeout(tempoId);
-   		game_over();
-   		return false;
- document.getElementByid('cronometro').innerHTML = segundos;
- tempoId = setTimeOut("contagem_tempo("+segundos")", 1000);
+    if(segundos == -1){
+        clearTimeout(timerId); 
+        game_over();
+        return false;
+    }
+
+    document.getElementById('cronometro').innerHTML = segundos;
+    timerId = setTimeout("contagem_tempo("+segundos+")", 1000);
 }
 
 function game_over(){
-	remove_balloon();
-	alert('TEMPO ESGOTADO, VOCÊ PERDEU!');
+    remove_eventos_baloes();
+    alert('Fim de jogo, você não conseguiu estourar todos os balões a tempo');
+}situacao_jogo:
+
+function cria_baloes(qtde_baloes){
+    for(var i = 1; i <= qtde_baloes; i++){
+        var balloon = document.createElement("img");
+        balloon.src = 'imagens/bp.png';
+        balloon.style.margin = '10px';
+        balloon.id = 'b'+i;
+        balloon.onclick = function() { estourar(this); }
+
+        document.getElementById('jogo').appendChild(balloon);
+
+    }
 }
-situacao_game:
 
-function criar_balloon(quantidade_balloon){
-	for(var i = 1; i <= quantidade_balloon; i++){
-		var balloon = document.createElement("img");
-  balloon.src = 'imagem/balloon_blue.png';
-  balloon.stilo.margin = '10px';
-  balloon.id = 'b'+i;
-   balloon.onclick = function() { estourar(this); }
+function estourar(e){
+    var id_balao = e.id;
+    document.getElementById(id_balao).setAttribute("onclick", "");
+    document.getElementById(id_balao).src = 'imagens/blp.png';
+    pontuacao(-1);
+}
 
-	document.getElementByid('game').appendChild(balloon);
-	}
+function pontuacao(acao){
+    var balloon_inteiros = document.getElementById('balloon_inteiros').innerHTML;
+    var balloon_estourados = document.getElementById('balloon_estourados').innerHTML;
+    
+    balloon_inteiros = parseInt(balloon_inteiros);
+    balloon_estourados = parseInt(balloon_estourados);
 
-	function estourar(e){
-		var id_ballon = e.id;
-		document.getElementByid(id_balloon).setAttribute("onclick", "");
-		document.getElementByid(id_balloon).src = 'imagem/pow.png';
-  		pontuacao(-1);
-	}
+    balloon_inteiros = balloon_inteiros + acao;
+    balloon_estourados = balloon_estourados - acao;
 
-	function pontuacao(acao){
-		var balloon_int = document.getElementByid('balloon_int').innerHTML;
-		var balloon_est = document.getElementByid('balloon_est').innerHTML;
+    document.getElementById('balloon_inteiros').innerHTML = balloon_inteiros;
+    document.getElementById('balloon_estourados').innerHTML = balloon_estourados;
 
-balloon_int = parseInt(balloon_int);
-balloon_est = parseInt(balloon_est);
+    situacao_jogo(balloon_inteiros);
+}
 
- balloon_int = balloon_int + acao;
- balloon_est = balloon_est - acao;
- document.getElementById('balloon_int').innerHTML = balloon_int;
- document.getElementById('balloon_est').innerHTML = balloon_est;
-
-    situacao_game(balloon_int);
- 
-	}
-
-	function situacao_game(balloon_int){
-    if(balloon_int == 0){
-        alert('PARABÉNS! VOCÊ CONSEGUIU ESTOURAR TODOS OS BALLOONS A TEMPO. UFAA');
-        stop_game();
+function situacao_jogo(balloon_inteiros){
+    if(balloon_inteiros == 0){
+        alert('Parabéns, você conseguiu estourar todos os balões e tempo');
+        parar_jogo();
     }
+}
 
-    function stop_game()
-    	clearIntervalo(tempoId);
+function parar_jogo(){
+    clearInterval(timerId);
+}
+
+function remove_eventos_baloes() {
+    var i = 1; //contado para recuperar balões por id
+    
+    //percorre o lementos de acordo com o id e só irá sair do laço quando não houver correspondência com elemento
+    while(document.getElementById('b'+i)) {
+        //retira o evento onclick do elemnto
+        document.getElementById('b'+i).onclick = '';
+        i++; 
     }
-
-    function remove_balloon_events(){
-    	var i = 1;
-
-    	while(document.getElementByid('b+i')){
-
-    		document.getElementByid('b'+).onclick = '';
-    		1++;
-    	}
-    }
+}
 
--- game.js
+++
@@ -1,4 +0,0 @@
-function playgame() {
-	var nivel = document.getElementByid('nivel').value;
-	window.location.href = 'balloon.html?' + nivel;
-}
